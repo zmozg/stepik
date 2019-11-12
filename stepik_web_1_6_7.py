d892bb1fbdced65c758e5616f407564c9567db91
Zmozg <42947177+zmozg@users.noreply.github.com>
2019-11-12 20:20:55 +0300

    Update stepik_web_1_6_7.py

--- stepik_web_1_6_7.py
+++ stepik_web_1_6_7.py
@@ -1,14 +1,26 @@
 import socket
 import os
 
+def handle_client(soc, conn):
+    while True:
+        data = soc.recv(1024)
+        if not data or data == 'close':
+            break
+        soc.send(data)
+        print(conn)
+    soc.close()
+
+            
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
-s.listen(10)
-child_pid = os.fork()
-if child_pid == 0:
-    while True:
-        sclient, addr = s.accept()
-        data = sclient.recv(1024)
-        if not data or data == 'close':
-            sclient.close()
-        sclient.send(data)
+s.listen(5)
+while True:
+   client = 1
+   while client <= 10:
+       child_pid = os.fork()
+       if child_pid == 0:
+           sclient, addr = s.accept()
+           handle_client(sclient, client)
+       else:
+           client += 1
