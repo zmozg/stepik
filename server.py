bcd07e9815143cf66ccd8607a1ca38f995929a84
Zmozg <42947177+zmozg@users.noreply.github.com>
2019-11-12 14:04:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,12 +4,13 @@ def server_receive(sock, msglen):
     msg = ''
     while len(msg) < msglen:
         chunk = sock.recv(msglen-len(msg))
-        if chuck = '':
+        if chunk == '':
+            sclient.close()
             raise RuntimeError('br')
         msg += chunk
     return msg
 
-s = socket.socket(socket.AF_INET, socet.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
 
@@ -19,6 +20,7 @@ while True:
         data = server_receive(sclient, 1024)
         if not data:
             break
+        if data == 'close':
+            sclient.close()
         conn.send(data)
-    if data == 'close':
-        sclient.close()
+    
