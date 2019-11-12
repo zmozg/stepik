392969aa354c6ab7ae5a7a5dfbc20136e8d695c0
Zmozg <42947177+zmozg@users.noreply.github.com>
2019-11-12 16:00:14 +0300

    Update stepik_web_1_6_7.py

--- stepik_web_1_6_7.py
+++ stepik_web_1_6_7.py
@@ -8,22 +8,21 @@ def handle_client(soc):
             break
         soc.send(data)
     soc.close()
-    return
 
 def server(s):
-   client = 1
-   while client <= 10:
-       sclient, addr = s.accept()
-       child_pid = os.fork()
-       if child_pid == 0:
-           handle_client(sclient)
-       else:
-           client += 1
+    while True:
+       client = 1
+       while client <= 10:
+           sclient, addr = s.accept()
+           child_pid = os.fork()
+           if child_pid == 0:
+               handle_client(sclient)
+           else:
+               client += 1
                
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(5)
 
-while True:
-    server(s)
+server(s)
