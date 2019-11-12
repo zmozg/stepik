38bb4502845d11425240f7e147a6a89da5bd30ea
Zmozg <42947177+zmozg@users.noreply.github.com>
2019-11-12 21:08:29 +0300

    Update stepik_web_1_6_7.py

--- stepik_web_1_6_7.py
+++ stepik_web_1_6_7.py
@@ -4,18 +4,17 @@ import os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(15)
+
 while True:
-    client = 1
     sclient, addr = s.accept()
     child_pid = os.fork()
     if child_pid == 0: 
         while True:
-            data = soc.recv(1024)
+            data = sclient.recv(1024)
             if not data or data == 'close':
                 break
             sclient.send(data)
-            client += 1
-            print(sclient.getpeername(), client, data, 'OK')
+            print(sclient.getpeername(), data, 'OK')
         sclient.close()   
     else:
         sclient.close()
