a1bbb0ac668d571160c2ac5b12703fe706dfdd67
zmozg <zmozg20@gmail.com>
2019-11-17 23:39:04 +0300

    2.2.6 work on lesson

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,4 +2,13 @@
 from django.http import HttpResponse
 
 def test(request, *args, **kwargs):
-    return HttpResponse('OK from qa')
+    response = HttpResponse('OK from qa or DJANGO')
+    #print(request.META)
+    #response['ATta'] = 'OK'
+    #response = request.META.get('HTTP_USER_AGENT')
+    #response = request.META.get('REMOTE_ADDR')
+    response.set_cookie('visited','OK')
+    print(request.COOKIES)
+
+    #return HttpResponse(response,)
+    return response
