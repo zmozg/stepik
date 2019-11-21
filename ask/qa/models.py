7ca3651988a43e858ac213bb50b24ff3404f655d
zmozg <zmozg20@gmail.com>
2019-11-21 16:50:50 +0300

    2.3 SQL in process

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_lenght=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, null=True, on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete = models.CASCADE)
--- init_SQL.sh
+++ init_SQL.sh
@@ -1,17 +1,14 @@
-DB_NAME = "ask"
-DB_USER_NAME = "box"
-
+#!/bin/bash
+DB_NAME="ask"
+DB_USER_NAME="box"
 # MYSQL
-
 #delete old settings
 #sudo rm -f /etc/mysql/conf.d/mysql.conf
 #Start MYSQL
-sudo /etc/init.d/mysql start
+#sudo /etc/init.d/mysql start
 #Create DataBase
 sudo mysql -u root -e "create database if not exists $DB_NAME;"
 #Create db_user with privileges to manage this database
 sudo mysql -u root -e "grant all privileges on $DB_NAME.* to\
     '$DB_USER_NAME'@'localhost' with grant option;"
-
-
 # add migrations self
