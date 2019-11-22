c4ecd642e7dd4abf3f49e0452bb3e39188691641
zmozg <zmozg20@gmail.com>
2019-11-22 20:51:14 +0300

    2.3 SQL in progress 2

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJ_DIR = os.path.dirname(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': PROJ_DIR + '/etc/mysql.cnf',
+            },
     }
 }
 
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.ordering(-'added_at')
+    def popular(self):
+        return self.ordering('rating')
+
 class Question(models.Model):
     title = models.CharField(max_lenght=255)
     text = models.TextField()
@@ -8,6 +15,7 @@ class Question(models.Model):
     rating = models.IntegerField()
     author = models.ForeignKey(User, null=True, on_delete = models.CASCADE)
     likes = models.ManyToManyField(User)
+    objects = QuestionManager()
 
 class Answer(models.Model):
     text = models.TextField()
