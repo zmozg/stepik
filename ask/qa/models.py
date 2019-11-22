ea14da7b7cc1ad168cbc8a1bca6897edf85f52b7
zmozg <zmozg20@gmail.com>
2019-11-22 21:18:09 +0300

    1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.ordering(-'added_at')
+        return self.order_by(-'added_at')
     def popular(self):
-        return self.ordering('rating')
+        return self.order_by('rating')
 
 class Question(models.Model):
     title = models.CharField(max_lenght=255)
