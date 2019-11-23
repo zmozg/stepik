0abb1c351bdd2fa4aa7b086a03da5b3dd17404d7
zmozg <zmozg20@gmail.com>
2019-11-23 23:24:21 +0300

    2.3 again

--- init_SQL.sh
+++ init_SQL.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DB_NAME="ask"
 DB_USER_NAME="box"
+WORK_DIR="/home/box"
+PROJ_DIR="$WORK_DIR/web"
+DJ_PROJ_DIR="$PROJ_DIR/ask"
 # MYSQL
 #delete old settings
 #sudo rm -f /etc/mysql/conf.d/mysql.conf
@@ -11,4 +14,7 @@ sudo mysql -u root -e "create database if not exists $DB_NAME;"
 #Create db_user with privileges to manage this database
 sudo mysql -u root -e "grant all privileges on $DB_NAME.* to\
     '$DB_USER_NAME'@'localhost' with grant option;"
-# add migrations self
+
+#make migrations
+$DJ_PROJ_DIR/manage.py makemigrations
+$DJ_PROJ_DIR/manage.py migrate
